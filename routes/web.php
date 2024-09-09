5fdda7d06ba8a4360a2562fa5d28fd98ba1748fe
Christian <chrisdaq@gmail.com>
2024-09-09 10:21:03 -0400

    add route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -22,6 +23,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    /* Students */
+    Route::get('/studentsdashboard', [StudentController::class, 'index'])->name('studentsdashboard.index');
+    Route::post('/addStudent', [StudentController::class, 'store'])->name('addStudent.store');
+    Route::patch('/updateStudent/{id}', [StudentController::class, 'update'])->name('updateStudent.update');
+    Route::delete('/deleteStudent/{id}', [StudentController::class, 'destroy'])->name('deleteStudent.destroy');
 });
 
 require __DIR__.'/auth.php';
